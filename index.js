4b8cac52ff5ef70a4255b40a0ee60c7a63e9d93e
Cory <corystephens012@gmail.com>
2021-07-22 12:08:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-function superbowlWin(array){
-   const found = array.find(winResults)
-   if(found){
-       return found.year
-   }
-//    else{
-//        return found.year
-//    }
-}
 
-winResults = function(element){
-    if (element.result === "W"){
-        return element.year
+function superbowlWin(record){
+    const getResult = record.find(season => season.result === "W")
+        if (getResult){
+            return getResult.year
     }
 }
