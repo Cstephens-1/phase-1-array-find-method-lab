c4e5de5cc2c2ae5422e077fd425b8a315eb41577
Cory <corystephens012@gmail.com>
2021-07-21 20:34:02 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(array){
+   const found = array.find(winResults)
+   if(found){
+       return found.year
+   }
+//    else{
+//        return found.year
+//    }
+}
 
+winResults = function(element){
+    if (element.result === "W"){
+        return element.year
+    }
+}
